14c995811fc8f5705cf6d2e34c23376ae9f0fee4
suzymalshyti <smalshyt@nd.edu>
2026-09-21 23:39:40 -0400

    streamlit app

---
+++  basic_streamlit_app/main.py
@@ -0,0 +1,35 @@
+import streamlit as st 
+import pandas as pd 
+
+st.title ("Palmer's Penguins Explorer")
+
+st.write("This app allows users to explore the Palmer's Penguins dataset by filtering penguin species, island,and body mass")
+
+df = pd.read_csv("penguins.csv")
+
+species = st.multiselect(
+    "Select species",
+    df["species"].unique()
+)
+
+results = df[df["species"] == species]
+
+
+island = st.multiselect(
+    "Select island",
+    df["island"].unique()
+)
+
+results = results[results["island"] == island]
+
+
+body_mass = st.slider(
+    " Slide Me! Select maximum body mass (grams)",
+    min_value=2000,
+    max_value=7000
+)
+
+results = results[results["body_mass_g"] <= body_mass]
+
+st.dataframe(results) 
+
